5ac51bd01058a31ca8c0f31d9c8d63182ff085c3
MariaCascone66 <mariacascone596@gmail.com>
2025-07-16 09:48:57 +0200

    aggiunta del main al gitignore

--- pybibx/base/main.py
+++ pybibx/base/main.py
@@ -44,5 +44,5 @@ scimago_source = ScimagoSource(scimago_rankings)
 vrc = VenueRankChecker(core_source, scimago_source)
 
 # Esempio di utilizzo
-venue_name = "Annual Review of Biochemistry"
+venue_name = "Information Retrieval Facility Conference"
 vrc.check_all(venue_name)
