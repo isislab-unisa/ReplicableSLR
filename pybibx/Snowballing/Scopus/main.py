9341163695b02a725209fcf826957857cf38869b
MariaCascone66 <mariacascone596@gmail.com>
2025-07-18 12:52:04 +0200

    eliminazione Snowballing

--- pybibx/Snowballing/Scopus/main.py
+++
@@ -1,9 +0,0 @@
-from pybibx.Snowballing.Scopus.snawball import SnowballingEngine
-
-engine = SnowballingEngine(api_key="YOUR_SCOPUS_API_KEY")
-data = engine.snowball("2-s2.0-85085004285")  # EID
-
-print("Title:", data['title'])
-print("Total Citations:", data['citations'])
-print("Forward (citanti):", len(data['forward']))
-print("Backward (citati):", len(data['backward']))
--- pybibx/__init__.py
+++ pybibx/__init__.py
@@ -1,1 +1,3 @@
+import pybibx.Snowballing.Scopus.client as mod
 
+print(dir(mod))
